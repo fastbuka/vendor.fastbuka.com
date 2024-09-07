aad5ddb935dd294130624ec7369db378fca92b60
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-09-07 21:33:33 +0100

    Modified navbar

--- fast-buka/src/pages/index.tsx
+++ fast-buka/src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../public/images/logo.png"
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,22 +12,28 @@ const Navbar = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <a href="#" className="text-xl font-bold text-gray-800">
-                Fast-Buka
-              </a>
+            <Link href="/">
+              <Image
+                src={Logo}
+                className="img-fluid"
+                alt="Fastbuka"
+                width={70}
+                height={70}
+              />
+            </Link>
             </div>
 
             {/* Desktop Menu */}
             <div className="flex md:order-2 space-x-3 md:space-x-3 rtl:space-x-reverse hidden md:block">
               <button
                 type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 drop-shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-indigo-500 duration-300 hover:drop-shadow-2xl"
+                className="text-white bg-[#045781] border border-[#045781] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 drop-shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-white hover:text-[#045781] duration-300 hover:drop-shadow-2xl"
               >
                 Login
               </button>
               <button
                 type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 drop-shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-indigo-500 duration-300 hover:drop-shadow-2xl"
+                className="text-white bg-[#045781] border border-[#045781] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-full text-sm px-4 py-2 text-center drop-shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-white hover:text-[#045781] duration-300 hover:drop-shadow-2xl"
               >
                 Register
               </button>
@@ -33,11 +42,11 @@ const Navbar = () => {
               className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
               id="navbar-sticky"
             >
-              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <ul className="flex flex-col p-4 md:p-0 mt-4 font-semibold border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
                   <a
                     href="#"
-                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-[#045781] md:p-0 md:dark:text-blue-500 font-semibold"
                     aria-current="page"
                   >
                     Home
@@ -46,7 +55,7 @@ const Navbar = () => {
                 <li>
                   <a
                     href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#045781] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-semibold"
                   >
                     About
                   </a>
@@ -54,7 +63,7 @@ const Navbar = () => {
                 <li>
                   <a
                     href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#045781] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-semibold"
                   >
                     Services
                   </a>
@@ -62,7 +71,7 @@ const Navbar = () => {
                 <li>
                   <a
                     href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#045781] md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-semibold"
                   >
                     Contact
                   </a>
@@ -74,7 +83,7 @@ const Navbar = () => {
             <div className="md:hidden">
               <button
                 onClick={() => setIsOpen(!isOpen)}
-                className="text-gray-800 hover:text-gray-400 focus:outline-none"
+                className="text-gray-800 hover:text-[#045781] focus:outline-none"
               >
                 <svg
                   className="w-6 h-6"
@@ -105,41 +114,41 @@ const Navbar = () => {
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               <a
                 href="#"
-                className="block text-gray-800 px-3 py-2 rounded-md text-base font-medium hover:bg-gray-200"
+                className="block text-gray-800 px-3 py-2 rounded-md text-base font-semibold hover:bg-gray-200"
               >
                 Home
               </a>
               <a
                 href="#"
-                className="block text-gray-800 px-3 py-2 rounded-md text-base font-medium hover:bg-gray-200"
+                className="block text-gray-800 px-3 py-2 rounded-md text-base font-semibold hover:bg-gray-200"
               >
                 About
               </a>
               <a
                 href="#"
-                className="block text-gray-800 px-3 py-2 rounded-md text-base font-medium hover:bg-gray-200"
+                className="block text-gray-800 px-3 py-2 rounded-md text-base font-semibold hover:bg-gray-200"
               >
                 Services
               </a>
               <a
                 href="#"
-                className="block text-gray-800 px-3 py-2 rounded-md text-base font-medium hover:bg-gray-200"
+                className="block text-gray-800 px-3 py-2 rounded-md text-base font-semibold hover:bg-gray-200"
               >
                 Contact
               </a>
               <div className="flex">
-                <button
-                  type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 drop-shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-indigo-500 duration-300 hover:drop-shadow-2xl lg:hidden block me-2"
-                >
-                  Login
-                </button>
-                <button
-                  type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 drop-shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-indigo-500 duration-300 hover:drop-shadow-2xl lg:hidden block ms-2"
-                >
-                  Register
-                </button>
+              <button
+                type="button"
+                className="text-white bg-[#045781] border border-[#045781] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 drop-shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-white hover:text-[#045781] duration-300 hover:drop-shadow-2xl me-3"
+              >
+                Login
+              </button>
+              <button
+                type="button"
+                className="text-white bg-[#045781] border border-[#045781] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-full text-sm px-4 py-2 text-center drop-shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-white hover:text-[#045781] duration-300 hover:drop-shadow-2xl"
+              >
+                Register
+              </button>
               </div>
             </div>
           </div>
