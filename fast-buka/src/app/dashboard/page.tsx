78f9f7f452f887c01834dabd197d10b7ff084ce8
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-09-13 11:23:52 +0100

    Completed Dashboard layout

---
+++ fast-buka/src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import ECommerce from "@/components/Dashboard/E-commerce";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title: "FastBuka | Dashboard",
+};
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <ECommerce />
+      </DefaultLayout>
+    </>
+  );
+}
