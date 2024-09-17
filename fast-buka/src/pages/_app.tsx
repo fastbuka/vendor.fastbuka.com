1ca63c6c2dadf9cbfda5b2c91f9f64c67975bcdf
Younglad-Paul <borisadedayopaul@gmaol.com>
2024-09-17 15:20:06 +0100

    Removed the AuthProvider wrapper from the _app.tsx file

--- fast-buka/src/pages/_app.tsx
+++ fast-buka/src/pages/_app.tsx
@@ -1,11 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { AuthProvider } from "../utils/AuthProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
       <Component {...pageProps} />
-    </AuthProvider>
   )
 }
