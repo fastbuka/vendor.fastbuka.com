7b6304871138002d808516bbe029b53441e51c59
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-09-30 07:50:00 +0100

    imported link to partners section

--- src/components/homepage/Partner.jsx
+++ src/components/homepage/Partner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import handshake from "../../../public/images/homepage/handshake.png";
 
 export default function Partner() {
