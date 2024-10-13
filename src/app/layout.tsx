28c73ed367f60e2ee45b3a938b16202adde328c6
Teslim Abdulwahab <tesimune@gmail.com>
2024-10-13 08:05:09 +0100

    Refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,23 @@
-"use client";
-import "jsvectormap/dist/jsvectormap.css";
-import "flatpickr/dist/flatpickr.min.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "@/css/satoshi.css";
 import "@/css/style.css";
-import React, { useEffect, useState } from "react";
-import Loader from "@/components/common/Loader";
-import { AuthProvider } from "@/utils/AuthProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "FastBuka",
+  description: "Your No. 1 food delivery App",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  // const pathname = usePathname();
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
-
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}>
-        <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          <AuthProvider >
-            {loading ? <Loader /> : children}
-          </AuthProvider>
-        </div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
