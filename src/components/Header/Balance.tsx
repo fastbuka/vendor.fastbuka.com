9a2d8e4acbbcb3ab2b1f400a3fe73020b7f5891a
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-10-23 20:34:32 +0100

    update

--- src/components/Header/Balance.tsx
+++ src/components/Header/Balance.tsx
@@ -18,39 +18,39 @@ const Balance = () => {
         </button>
         <div className="grid grid-cols-3 gap-10">
           <div className="flex items-center justify-center ">
-            <h1 className="font-mono font-black text-sm ms-2">Total: </h1>
+            <h1 className="font-mono font-black text-sm 2xl:text-xl ms-2">Total: </h1>
             <input
               type={balance ? "password" : "text"}
               id="password"
               name="password"
               value="3,800,000"
-              className="font-mono font-black text-sm ms-2 block w-1/2 bg-white"
+              className="font-mono font-black text-sm 2xl:text-xl ms-2 block w-1/2 bg-white"
               placeholder="Password"
               required
               disabled
             />
           </div>
           <div className="flex items-center justify-center border-l-2 border-[#3ab764] ">
-            <h1 className="font-mono font-black text-sm ms-2">NGN: </h1>
+            <h1 className="font-mono font-black text-sm 2xl:text-xl ms-2">NGN: </h1>
             <input
               type={balance ? "password" : "text"}
               id="password"
               name="password"
               value="1,900,000"
-              className="font-mono font-black text-sm ms-2 block w-1/2 bg-white"
+              className="font-mono font-black text-sm 2xl:text-xl ms-2 block w-1/2 bg-white"
               placeholder="Password"
               required
               disabled
             />
           </div>
           <div className="flex items-center justify-center border-l-2 border-[#3ab764] mx-auto">
-          <h1 className="font-mono font-black text-sm ms-2">NGNC: </h1>
+          <h1 className="font-mono font-black text-sm 2xl:text-xl ms-2">NGNC: </h1>
             <input
               type={balance ? "password" : "text"}
               id="password"
               name="password"
               value="1,900,000"
-              className="font-mono font-black text-sm ms-2 block w-1/2 bg-white"
+              className="font-mono font-black text-sm 2xl:text-xl ms-2 block w-1/2 bg-white"
               placeholder="Password"
               required
               disabled
